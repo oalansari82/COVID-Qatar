e3687c5f54c949e6c7063eaf62f2c92b893e9b92
Omar Al-Ansari <oalansari@gmail.com>
2021-04-12 06:55:40 +0300

    Added animation to barChart

--- COVID Qatar/Views/BarChart.swift
+++ COVID Qatar/Views/BarChart.swift
@@ -11,47 +11,55 @@ import SwiftUI
 struct BarChart: View {
     
     @ObservedObject var lvm = ListViewModel()
+    @State var animateChart = false
     
     var body: some View {
         VStack {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(alignment: .bottom) {
                     ForEach(lvm.data.prefix(90), id: \.self) { record in
-                        
+                        VStack {
+                            Spacer()
                             Rectangle()
-                                .frame(width: 20, height: (CGFloat(record.numberOfNewTestsInLast24Hrs ?? 0) / self.lvm.getMaxTests()) * 300)
-                                .foregroundColor(Color(.systemGray3))
-                                .overlay(
-                                    VStack(spacing: 0) {
-                                        Rectangle().fill(Color.blue)
-                                            .frame(height: (CGFloat(record.numberOfNewPositiveCasesInLast24Hrs ?? 0) / self.lvm.getMaxTests()) * 300)
-                                        
-                                        Rectangle().fill(Color.red)
-                                            .frame(height: (CGFloat(record.numberOfNewDeathsInLast24Hrs ?? 0) / self.lvm.getMaxTests()) * 10000)
-
-                                        Text("\(record.numberOfNewPositiveCasesInLast24Hrs ?? 0)")
-                                            .font(.caption2)
-                                            .rotationEffect(.degrees(-90))
-                                            .frame(width: 70)
-                                            .offset(y: -45)
-                                        
-                                        Spacer()
-                                        Text("\(record.numberOfNewTestsInLast24Hrs ?? 0)")
-                                            .font(.caption2)
-                                            .rotationEffect(.degrees(-90))
-                                            .frame(width: 70)
+                                    .frame(width: 20, height: animateChart ? (CGFloat(record.numberOfNewTestsInLast24Hrs ?? 0) / self.lvm.getMaxTests()) * 300 : 0)
+                                    .foregroundColor(Color(.systemGray3))
+                                    .overlay(
+                                        VStack(spacing: 0) {
+                                            Rectangle().fill(Color.blue)
+                                                .frame(height: animateChart ? (CGFloat(record.numberOfNewPositiveCasesInLast24Hrs ?? 0) / self.lvm.getMaxTests()) * 300 : 0)
                                             
-                                        Spacer()
-                                        Text("\(record.date ?? "")")
-                                            .font(.caption2)
-                                            .rotationEffect(.degrees(-90))
-                                            .frame(width: 70)
-                                            .offset(y: 45)
-                                            .lineLimit(1)
-                                            .minimumScaleFactor(0.5)
-                                    }
-                                    
-                            )
+                                            Rectangle().fill(Color.red)
+                                                .frame(height: animateChart ? (CGFloat(record.numberOfNewDeathsInLast24Hrs ?? 0) / self.lvm.getMaxTests()) * 10000 : 0)
+
+                                            Text("\(record.numberOfNewPositiveCasesInLast24Hrs ?? 0)")
+                                                .font(.caption2)
+                                                .rotationEffect(.degrees(-90))
+                                                .frame(width: 70)
+                                                .offset(y: -45)
+                                            
+                                            Spacer()
+                                            Text("\(record.numberOfNewTestsInLast24Hrs ?? 0)")
+                                                .font(.caption2)
+                                                .rotationEffect(.degrees(-90))
+                                                .frame(width: 70)
+                                                
+                                            Spacer()
+                                            Text("\(record.date ?? "")")
+                                                .font(.caption2)
+                                                .rotationEffect(.degrees(-90))
+                                                .frame(width: 70)
+                                                .offset(y: 45)
+                                                .lineLimit(1)
+                                                .minimumScaleFactor(0.5)
+                                        }
+                                        
+                                )
+                                    .onAppear {
+                                        withAnimation(.easeInOut(duration: 1)) {
+                                            animateChart = true
+                                        }
+                                }
+                        }
                     }
                 }.frame(height: 450)
             }
